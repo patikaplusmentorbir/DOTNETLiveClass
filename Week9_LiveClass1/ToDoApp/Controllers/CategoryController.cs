2b139f78f06f785d3c231be796f98f0c8bde6edd
sertanbozkus <sertanbozkus@patika.dev>
2025-07-01 21:17:10 +0300

    Add files via upload

---
+++ Week9_LiveClass1/ToDoApp/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    // Categorylerle ilgili CRUD işlemlerini toplayacağım temel çatı.
+    // CRUD -> Create, Read, Update, Delete
+    public class CategoryController : Controller
+    {
+        // Veritabanı bağlantım olmadığı için statik bir liste kullancağım. Buna InMemoryDB denebilir.
+
+        public static List<Category> categories = new List<Category>
+        {
+            new Category{ Id=1, Name = "İş", Description = "İş hayatına dair görevlerin bulunduğu kategori."},
+            new Category{ Id=2, Name = "Aile", Description = "Aile hayatına dair görevlerin bulunduğu kategori."}
+        };
+
+
+        [HttpGet] // Link üzerinden tetiklenen.
+        public IActionResult Add() 
+        {
+            return View();
+        }
+
+        [HttpPost] // Formdaki buttondan tetikleniyor.
+        public IActionResult Add(CategoryAddViewModel formData) 
+        {
+            var newCategory = new Category()
+            {
+                Id = categories.Max(x => x.Id) + 1,
+                Name = formData.Name,
+                Description = formData.Description
+            };
+
+            categories.Add(newCategory);
+
+
+            return Ok();
+        }
+    }
+}
