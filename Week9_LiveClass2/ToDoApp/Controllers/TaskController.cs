f30bfb4de0ae188b1f23cb63ee3d8687111dd0cf
sertanbozkus <sertanbozkus@patika.dev>
2025-07-03 21:03:44 +0300

    Add files via upload

---
+++ Week9_LiveClass2/ToDoApp/Controllers/TaskController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    public class TaskController : Controller
+    {
+        public static List<TaskItem> tasks = new List<TaskItem>
+        {
+            new TaskItem { Id = 1, Name = "Ailenle Sinemaya git" , Description = "Son çıkan popüler filmlerden bir tanesine gidip kendine zaman ayır.", CategoryId = 2}
+        };
+
+        public static List<Category> categories = new List<Category>
+        {
+            new Category{ Id=1, Name = "İş", Description = "İş hayatına dair görevlerin bulunduğu kategori."},
+            new Category{ Id=2, Name = "Aile", Description = "Aile hayatına dair görevlerin bulunduğu kategori."}
+        };
+
+        // Db bağlantısı olduğunda veriler Db'den çekileceğinden yukarıdaki statik listeler kullanılmayacak.
+
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Categories = categories.Select(x => new TaskAddCategoryViewModel
+            {
+                Id = x.Id,
+                CategoryName = x.Name,
+            }).OrderBy(x => x.CategoryName).ToList();
+        
+            return View();
+        }
+
+
+      
+    }
+}
---
+++ Week9_LiveClass2/ToDoApp/Models/CategoryAddViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoApp.Models
+{
+
+    // Category ekleme işlemleri sırasında view ile controller arasında veri taşıyacak olan model.
+    public class CategoryAddViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
