351cff832a567a64040d786867b3ba0588a677ec
sertanbozkus <sertanbozkus@patika.dev>
2025-07-07 19:58:53 +0300

    Add files via upload

---
+++ Week4_LiveClass1/OOP_Giris/Kisi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Giris
+{
+    // Nesne oluşturma şablonu ( NESNENIN KENDISI DEĞİL !!! ) 
+    internal class Kisi
+    {
+        // Her classın içerisinde en az 1 adet yapıcı metot bulunur. (Constructor)
+
+        // Nesne oluşturulurken zorunlu ve otomatik olarak 1 kere tetiklenir.
+        public Kisi() // Default Constructor
+        {
+            Console.WriteLine("Kişi nesnesi oluşturuluyor.");
+        }
+
+        public Kisi(string ad, string soyad) // Alternatif Constructor
+        {
+            Ad = ad;
+            Soyad = soyad;
+            // BÜYÜK HARFLİLER YANİ DEĞER ATANANLAR -> PROPERTY
+            // küçük harfliler -> metot parametreleri
+        }
+
+        // Aynı isimli constructordan birden fazla alternatif oluşturmam -> Method Overloading ( Metotların aşırı yüklenmesi )
+
+
+        public string Ad { get; set; } // Aynı değişken gibi veri tutmaya yarar. PascalCasing isimlendirilir.
+
+        public string Soyad { get; set; }
+
+        public int Yas { get; set; }
+
+        public void KendiniTanit()
+        {
+            Console.WriteLine("Merhaba ben " + Ad + " " + Soyad); // Her kişi kendi property alanında tutulan değeri söylecek. O nedenle kisi1.Ad - kisi2.Soyad gibi kullanım class içerisinde yok.
+        }
+    }
+}
---
+++ Week4_LiveClass1/OOP_Giris/Program.cs
@@ -0,0 +1,79 @@
+﻿
+//string ad1 = "Ajda";
+//string soyad1 = "Pekkan";
+//int yas1 = 75;
+
+//string ad2 = "Tarkan";
+//string soyad2 = "Tevetoğlu";
+//int yas = 40;
+
+
+
+
+
+using OOP_Giris;
+using System.Reflection.Metadata;
+
+Kisi kisi1 = new Kisi(); // New'lemek - Nesne Oluşturmak - Instance almak. -> Hepsi aynı.
+
+kisi1.Ad = "Ajda";
+kisi1.Soyad = "Pekkan";
+kisi1.Yas = 75;
+
+// Daha pratik yoldan nesne oluşturma  ve değer atama, her defasında kisi2. yazmaya gerek duymuyoruz.
+Kisi kisi2 = new Kisi
+{
+    Ad = "Tarkan",
+    Soyad = "Tevetoğlu",
+    Yas = 45
+};
+
+// 2 yöntemde de algoritmik sırayla -> Önce nesne oluşturulur sonra değerlera atandı.
+
+// Bir property'e değer atarkan içerisindeki "set" metodunu tetiklersin.
+
+Console.WriteLine("Davetimize hoş geldiniz.");
+
+Console.WriteLine("Davetimizin ilk sanatçısının bilgiler:");
+Console.WriteLine("Ad --> " + kisi1.Ad);
+Console.WriteLine("Soyad --> " + kisi1.Soyad );
+// Değerleri okuma aşamasında propertylerin (Ad ve Soyad propertylerinin) Get metotları tetiklendi.
+
+Kisi kisi3 = new Kisi
+{
+    Ad = "Sertan",
+    Soyad = "Bozkuş",
+    Yas = 29
+};
+
+
+kisi1.KendiniTanit(); //  Koruma  seviyesi yüzünden erişilmiyor hatası alırsam bu erişim belirteçleriyle oynamam gerektiği anlamına gelir. Public yaptığım için artık erişebilirim.
+
+Console.WriteLine("---");
+
+kisi2.KendiniTanit();
+
+Console.WriteLine("---");
+
+kisi3.KendiniTanit();
+
+Console.WriteLine("-----------------------------------------------------------");
+
+Kisi denemeKisisi = new Kisi();
+Kisi denemeKisisi2 = new Kisi();
+Kisi denemeKisisi3 = new Kisi();
+Kisi denemeKisisi4 = new Kisi();
+
+
+Kisi yeniKisi = new Kisi("Pınar", "Yetiş");
+
+
+
+// Bu noktadan itibaren bu projeyi kullananın 2 şekilde kişi  oluşturma imkanı var.
+
+// Ya önce boş değerleri olan bir kişi nesnesi oluşturacak ve 2. aşamada değerleri atayacak propertylere
+
+// Ya da nesneyi oluştururken ad ve soyad bilgilerini verip, propertyleri dolu bir şekilde oluşturacak.
+
+// Ad, Soyad, Yaş propertyleri dolu nesne oluşturmaya sağlayan 3. constructor seçeneğini de siz yazın.
+
